e2b7f11ea35aa9ec021fd56bf7e454f1c02d89b2
Raihanah1312 <hanaraihanah13@gmail.com>
2025-05-21 15:12:31 +0700

    rapiin file

--- server_utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "server_utils.h"
-#include <iostream>
-
-void log_message(const std::string& message) {
-    std::cout << "[SERVER] " << message << std::endl;
-}
--- server_utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef SERVER_UTILS_H
-#define SERVER_UTILS_H
-
-#include <string>
-void log_message(const std::string& message);
-
-#endif
